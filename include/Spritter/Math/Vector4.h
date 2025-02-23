e6446d98645d112bf752a21f6b8e7fe81196e595
Aqua <ollie@ollierobinson.co.uk>
2025-02-23 14:27:11 +0000

    basic vector4 struct

---
+++ include/Spritter/Math/Vector4.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <cstdint>
+
+namespace Spritter::Math
+{
+    template <typename T>
+    struct Vector4
+    {
+        T X;
+        T Y;
+        T Z;
+        T W;
+
+        Vector4()
+        {
+            X = 0;
+            Y = 0;
+            Z = 0;
+            W = 0;
+        }
+
+        explicit Vector4(const T scalar)
+        {
+            X = scalar;
+            Y = scalar;
+            Z = scalar;
+            W = scalar;
+        }
+
+        Vector4(const T x, const T y, const T z, const T w)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+            W = w;
+        }
+
+        static T Dot(const Vector4& a, const Vector4& b)
+        {
+            return a.X * b.X + a.Y * b.Y + a.Z * b.Z + a.W * b.W;
+        }
+    };
+
+    using Vector4f = Vector4<float>;
+    using Vector4d = Vector4<double>;
+    using Vector4i = Vector4<int32_t>;
+}
