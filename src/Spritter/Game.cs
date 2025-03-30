c2bfff33e2299411022081625778c380222147cf
Aqua <ollie@ollierobinson.co.uk>
2025-03-30 15:50:56 +0100

    replace window with pie.windowing

--- src/Spritter/Game.cs
+++ src/Spritter/Game.cs
@@ -1,16 +1,47 @@
 using Pie;
+using Pie.Windowing;
+using Pie.Windowing.Events;
 
 namespace Spritter;
 
 public abstract class Game : IDisposable
 {
+    private bool _isRunning;
+
     public readonly Window Window;
     
     public Game(in GameOptions options)
     {
-        GraphicsApi api = GraphicsDevice.GetBestApiForPlatform();
+        Window = new WindowBuilder()
+            .Title(options.Name)
+            .Size(options.Size.Width, options.Size.Height)
+            .Build(out GraphicsDevice device);
+    }
 
-        Window = new Window(options.Name, options.Size, api);
+    public void Run()
+    {
+        if (_isRunning)
+            return;
+
+        _isRunning = true;
+
+        while (_isRunning)
+        {
+            while (Window.PollEvent(out IWindowEvent winEvent))
+            {
+                switch (winEvent)
+                {
+                    case QuitEvent:
+                        _isRunning = false;
+                        break;
+                }
+            }
+        }
+    }
+
+    public void Close()
+    {
+        _isRunning = false;
     }
     
     public virtual void Dispose()
